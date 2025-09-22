13ab84849175957685c88102d3bfb601a083e232
Rich Haase <haaserdh@gmail.com>
2025-09-22 09:17:20 -0600

    chore: add release prep helper

---
+++ scripts/publishPrepMessage.mjs
@@ -0,0 +1,5 @@
+console.log('\nPublish prep complete. Next steps:');
+console.log('  * If templates changed: bun run snapshots:generate');
+console.log('  1. npm version <major|minor|patch>');
+console.log('  2. npm publish');
+console.log('  3. git push origin main --tags');
